c43dfd849f4e527d16b6693696e88f2cea668798
laakkaj <113774613+laakkaj@users.noreply.github.com>
2022-09-17 20:11:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxcbd96d19fab6246d",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "12cbe792b96f44caac2a0bb6527c0876",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "马子轩爱张紫玉",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "BvbFhnhMYKdot6i1PXAX17xuzZhMSg8Wv37poAiYEiY",
       // 所在省份
       province: "广东",
       // 所在城市
